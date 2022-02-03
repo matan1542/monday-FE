f27c270678aeadf4aad1bd2c8af2930564db31f0
Lihi <lihise9@gmail.com>
2022-02-03 15:44:39 +0200

    Conflict solved

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,6 @@ import React, { Fragment, useEffect, useState } from 'react';
 import Home from './pages/Home'
 import { Header } from './components/Header';
 import { Routes, Route, Router } from "react-router-dom";
-<<<<<<< HEAD
-import { RecoilRoot, atom, selector, useRecoilState, useRecoilValue } from 'recoil';
-=======
 import {
   RecoilRoot,
   atom,
@@ -14,32 +11,21 @@ import {
   useRecoilValue,
 } from 'recoil';
 import SideBar from './components/SideBar';
->>>>>>> 521ab8cc3c27effbefcf6f70a9cd4435b68edf2b
 function App() {
 
   return (
     <RecoilRoot>
       <div className="app">
-<<<<<<< HEAD
         <main className="layout-main-container">
           <section className="layout-page-container">
+            <SideBar />
             <Routes>
-              <Route path="/home" element={<Home />}></Route>
+              <Route path="/" element={<Home />}></Route>
             </Routes>
           </section>
         </main>
-=======
-          <main className="layout-main-container">
-            <section className="layout-page-container">
-            <SideBar />
-              <Routes>
-                <Route path="/" element={<Home />}></Route>
-              </Routes>
-            </section>
-          </main>
->>>>>>> 521ab8cc3c27effbefcf6f70a9cd4435b68edf2b
-      </div>
-    </RecoilRoot>
+      </div >
+    </RecoilRoot >
   );
 }
 

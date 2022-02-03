1edf5f8304d42341e4fd0245d9bd6becfb7d58a8
matan1542 <matan8380@gmail.com>
2022-02-03 15:27:41 +0200

    Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './style/style.scss';
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import Home from './pages/Home'
 import { Header } from './components/Header';
 import { Routes, Route, Router } from "react-router-dom";
@@ -11,13 +11,14 @@ import {
   useRecoilValue,
 } from 'recoil';
 function App() {
+
   return (
     <RecoilRoot>
       <div className="app">
           <main className="layout-main-container">
             <section className="layout-page-container">
               <Routes>
-                <Route path="/home" element={<Home />}></Route>
+                <Route path="/" element={<Home />}></Route>
               </Routes>
             </section>
           </main>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,23 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import { useRecoilState } from 'recoil'
 
 export default function Home() {
-
+    const [num, setNum] = useState(0)
+    useEffect(() => {
+        console.log('isNumber', num);
+        // return ()=> destroy
+    },[])
+    const calcNum = useMemo(() => {
+        return num / 3
+    }, [num])
+    const clickMe = () => {
+        let sum = num + 10
+        setNum(sum)
+    }
     return (
         <div className="home-container">
-            <h2>Hello</h2>
+            <button onClick={clickMe}>Click!</button>
+            <h2>Hello: {calcNum}</h2>
         </div>
     )
 }

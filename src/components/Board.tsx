7f66b6dd75beb36c75416f2802493bea68ba637a
matan1542 <matan8380@gmail.com>
2022-02-06 14:13:07 +0200

    animation transition

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
-
-export default function Board() {
-  return <section className="board-container">
-      Board
-  </section>;
+type BoardProps = {
+    toggleSideBar:() => void,
+    classIsShowBoard:string
+}
+export default function Board({toggleSideBar,classIsShowBoard}:BoardProps) {
+  
+    return <section className={`board-container ${classIsShowBoard}`}>
+        <button onClick={toggleSideBar}>Toggle side-bar</button>
+        Board
+    </section>;
 }
--- src/pages/Workspace.tsx
+++ src/pages/Workspace.tsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
+import Board from '../components/Board';
+import SideBar from '../components/SideBar';
+import WorkspaceSideBar from '../components/WorkspaceSideBar';
 
 export default function Workspace() {
-  return <div>
-      workspace 
-  </div>;
+  const [isShow,setIsShow] = useState(false) 
+
+  const classIsShowWorkSpace = useMemo(()=>{
+    return isShow ? 'show-workspace' : '';
+  },[isShow])
+  const classIsShowBoard = useMemo(()=>{
+    return isShow ? 'slide-board' : '';
+  },[isShow])
+  const toggleSideBar = () => {
+    setIsShow(!isShow)
+  }
+  // const props = { classIsShow: classIsShow<};
+  return (<>
+    <SideBar />
+    <div className='app-container workspace-container'>
+      <WorkspaceSideBar classIsShowWorkSpace={classIsShowWorkSpace} />
+      <Board toggleSideBar={toggleSideBar} classIsShowBoard={classIsShowBoard}/>
+    </div>
+  </>)
 }
